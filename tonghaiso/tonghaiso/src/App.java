681bcb1930cc4cc3b3440f60aedb20f7f884f9c5
thuklhd <97714679+thuklhd@users.noreply.github.com>
2022-06-26 22:55:38 +0700

    Update App.java

--- tonghaiso/tonghaiso/src/App.java
+++ tonghaiso/tonghaiso/src/App.java
@@ -1,78 +1,99 @@
 import java.util.ArrayList;
-
-import javax.lang.model.util.ElementScanner14;
-
 public class App {
 
 
-
-
-  static String fill(String string,String b)
+//hàm làm đầy
+  static String fill(String string,int length)
   {
-    string=daoXau(string);
-   
-   
-    for (int i =0; i <=(b.length()- string.length()+25); i++) {
-      string=string+ "0";
+     String s1="";
+  int amount=length-string.length();
+    string=reverse(string);
+    for (int i =0; i <amount; i++) 
+    {
+      string=string+"0";
+     
     }
-    string=daoXau(string);
-  
+   
+    string=reverse(string);
     return string;
   }
 
 
 
 
+//hàm đảo ngược
+  static String reverse(String string) 
+  {
 
-  static String daoXau(String s) {
-    String s1 = "";
-    for (int i = 0; i < s.length(); i++) {
-        s1 += s.charAt(s.length() - 1 - i);
+    String result = "";
+    for (int i = 0; i < string.length(); i++) 
+    {
+      result += string.charAt(string.length() - 1 - i);
     }
-    return s1; 
-}
+    return result; 
+  }
  
 
 
 
 
-
+// hàm tính tổng
    static String total( String numbera, String numberb)
-   {
-    String s="";
-    int len1=numbera.length();
-    int len2=numberb.length();
-    if ( len2>len1) {
-    numbera= fill(numbera, numberb);
-  }
+     {
+    String result="";
+    //kiểm tra xem số nào ngắn hơn và làm đầy
+         if ( numbera.length()<numberb.length()) 
+           {
+           numbera= fill(numbera, numberb.length());
+           }
 
 
- else if (len1>len2 ) {
-  numberb=fill(numberb, numberb);
-  }
+         if (numbera.length()>numberb.length() ) 
+           {
+           numberb=fill(numberb, numbera.length());
+           }
 
   
-    int temp,temp2=0,sum;
-    int length=numberb.length() ;
-for(int i=numberb.length()-1;i>=0;i--)
-{
-sum=numbera.charAt(i)-'0'+numberb.charAt(i)-'0'+temp2;
-  temp=sum%10;
-  temp2=sum/10;
- s=s+temp;
- 
-}
- s=daoXau(s);
-return s;
+          int temp=0,mnemonicNumber=0,sum=0;
+          int length ;
+         if(numbera.length()>numberb.length())
+          {
+          length=numbera.length();
+          }
+          else
+          {
+           length=numberb.length();
+          }
+
+
+          for(int i=length-1;i>=0;i--)
+          {
+          sum=numbera.charAt(i)-'0'+numberb.charAt(i)-'0'+mnemonicNumber;
+          temp=sum%10;
+          mnemonicNumber=sum/10;
+          result=result+temp;
+          }
+
+          
+          result=reverse(result);
+
+
+return result;
    }
-   public static void main(String[] args) throws Exception {
+
+
+
+
+    public static void main(String[] args) throws Exception {
         String a="655555555555555555554365435555555555555544745765476547654765476476";
         String b="65555555465547654755543654365465555555555547654755554765755555555544745765476547654765476436";
-      
-      System.out.println(total(a, b));
+    
+      System.out.println("kết quả là "+total(a, b));
+     
      
      
       
     }
 }
+
 
